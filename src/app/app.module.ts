24ebb7848ef72f16be68eb2e153d229f2248bccd
ddresselhouse <ddresselhouse@gmail.com>
2019-10-24 15:05:19 -0400

    File upload to S3 finalized (Dan)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AngularFileUploaderModule } from "angular-file-uploader";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UploadComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
