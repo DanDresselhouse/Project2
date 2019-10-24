c1c1993a9491ad0f2c2644fa5a36378b7009c239
ddresselhouse <ddresselhouse@gmail.com>
2019-10-24 14:36:35 -0400

    Upload files to S3 bucket (Dan)

---
+++ src/services/upload.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+
+  constructor() { }
+
+  uploadFile(file) {
+    const contentType = file.type;
+    const bucket = new S3({
+      accessKeyId: 'AKIAUNBRJRRBG5IUQ3XG',
+      secretAccessKey: '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',
+      region: 'us-east-2'
+    });
+    const params = {
+      Bucket: 'songcollectionbucket',
+      Key: file.name,
+      Body: file,
+      ACL: 'public-read',
+      ContentType: contentType
+    }
+    bucket.upload(params, function(err, data) {
+      if (err) {
+        console.log('There was an error uploading the file: ', err);
+        return false;
+      }
+      console.log('Successfully uploaded file.', data);
+      return true;
+    });
+  }
+
+}
