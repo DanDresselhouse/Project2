9b4f1d358a64219b91c6bcf771224d1270ce5ca0
NathanJ2125 <nathanj2125@gmail.com>
2019-10-29 15:54:16 -0400

    added some stars and stuff

--- src/app/components/useroptionspage/useroptionspage.component.ts
+++ src/app/components/useroptionspage/useroptionspage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GetrequestService } from 'src/services/getrequest.service';
 import { Song } from 'src/app/models/song';
 import { Artist } from 'src/app/Models/artist';
+import { DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'app-useroptionspage',
@@ -9,10 +10,15 @@ import { Artist } from 'src/app/Models/artist';
   styleUrls: ['./useroptionspage.component.css']
 })
 export class UseroptionspageComponent implements OnInit {
-
-  constructor(private getrequest: GetrequestService) { }
+  userid:number = 0;
+  constructor(private getrequest: GetrequestService, private data:DataService) { }
 
   ngOnInit() {
+    
+    this.data.currentMessage.subscribe(id => this.userid=id)
+
+    this.getallartists();
+    this.getallsongs();
   }
   songs: Array<Song>;
   searchinput = "";
@@ -20,7 +26,8 @@ export class UseroptionspageComponent implements OnInit {
 
   //this logins the artist and sets the artist object
   search() {
-
+    this.artists = null;
+    this.songs = null;
     //backend endpoint goes here
     let url = "";
     this.getrequest.getmethod(url).then((info) => {
@@ -30,13 +37,20 @@ export class UseroptionspageComponent implements OnInit {
   }
 
   getallartists() {
-
     let url = "http://ec2-18-216-221-127.us-east-2.compute.amazonaws.com:9999/artist";
     console.log(url);
     this.getrequest.getmethod(url).then((info) => {
       console.log("success");
       this.artists = info;
       console.log(info);
+    }).catch((response) => { console.log("Information couldn't be found") });
+  }
+
+  getallsongs(){
+    let url = "http://ec2-18-216-221-127.us-east-2.compute.amazonaws.com:9999/song";
+    console.log(url);
+    this.getrequest.getmethod(url).then((info) => {
+      this.songs = info;
     }).catch((response) => { console.log("Information couldn't be found") });
   }
 }
